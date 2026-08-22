7b58feb9d383e38a3c06377a0d1c5d3b489870b9
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-08-22 22:21:01 +0530

    ✅ CBSPEED - Rating 288 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/easy/CBSPEED---Rating-288/solution.py
+++ codechef/easy/CBSPEED---Rating-288/solution.py
@@ -1,6 +1,9 @@
 x,y = map(int,input().split())
 # write your code here
-if x==y and :
-    print("NO")
+if  1 <=x and y<=100:
+    if x==y:
+        print("NO")
+    else:
+        print("YES")
 else:
-    print("YES")
+    print("NO")

28c19b2e91464e8c97d1c1fe9621903b286fe8e0
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-08-28 17:17:54 +0530

    ✅ LPYAS69 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-28

---
+++ codechef/medium/LPYAS69/solution.py
@@ -0,0 +1,8 @@
+n=int(input())
+if n%5==0:
+    if n%3==0:
+        print("Divisible by both 3 and 5")
+    else:
+        print("not divisible by both 3 and 5")
+else:
+    print("not divisible by both 3 and 5")

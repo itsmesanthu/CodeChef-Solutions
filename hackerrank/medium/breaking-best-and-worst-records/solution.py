5250ff67cc1e934fe607894a85bb69039cad180b
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-08-28 18:56:15 +0530

    ✅ Breaking the Records | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-28

--- hackerrank/medium/breaking-best-and-worst-records/solution.py
+++ hackerrank/medium/breaking-best-and-worst-records/solution.py
@@ -7,38 +7,35 @@ import re
 import sys
 
 #
-# Complete the 'getTotalX' function below.
+# Complete the 'breakingRecords' function below.
 #
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY a
-#  2. INTEGER_ARRAY b
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
 #
 
-def getTotalX(a, b):
-    count = 0
-    for x in range(1, 101):
-        if all(x % num == 0 for num in a):
-            if all(num % x == 0 for num in b):
-                count += 1
-
-    return count
-
+def breakingRecords(scores):
+    mi=scores[0]
+    mx=scores[0]
+    cmi=0
+    cmx=0
+    for i in scores:
+        if i<mi:
+            mi=i
+            cmi+=1
+        if i>mx:
+            mx=i
+            cmx+=1
+    return cmx,cmi
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    first_multiple_input = input().rstrip().split()
+    n = int(input().strip())
 
-    n = int(first_multiple_input[0])
+    scores = list(map(int, input().rstrip().split()))
 
-    m = int(first_multiple_input[1])
+    result = breakingRecords(scores)
 
-    arr = list(map(int, input().rstrip().split()))
-
-    brr = list(map(int, input().rstrip().split()))
-
-    total = getTotalX(arr, brr)
-
-    fptr.write(str(total) + '\n')
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
 
     fptr.close()

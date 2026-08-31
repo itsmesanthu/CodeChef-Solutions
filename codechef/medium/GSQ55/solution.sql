e7a0ed4f41ddb1fcc069aeee3b3927552ea6749b
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-08-31 20:19:42 +0530

    ✅ GSQ55 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/GSQ55/solution.sql
+++ codechef/medium/GSQ55/solution.sql
@@ -1,15 +1,7 @@
-/* Write a query to add an additional column 'email' to the table 'customer'.
-Consider the table 'customer' to have Id, Name, Age, Address as existing columns*/
-
--- Alter the existing table to add the column 'email'
-Alter table customer
-add email text;
-
--- Insert into 'customer' all the values for the corresponding columns
-insert into customer(Id,Name,Age,Address,email)
-
-VALUES(1, 'John Smith', 25, 'Main St','john@ex.com'),
-(2, 'Sarah Johnson', 30,'Broadway','sarah@ex.com');
-
--- Output the entire table to the console
-select * from customer
+/* Write a query which does the following
+- Update the age to 52 for the customer with customer id 3.
+- Output all the entires of the table. */
+Update customer 
+set age=52
+where id=3;
+select * from customer;

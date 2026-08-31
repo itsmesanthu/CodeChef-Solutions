e6b537b2231101616f4d8b0402abce34ee6cee27
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-08-31 20:20:32 +0530

    ✅ GSQ56 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/GSQ56/solution.sql
@@ -0,0 +1,6 @@
+/*Write a query which does the following
+- Delete the entire row of the customer with customer id 1.
+- Output the entire table once the above action is performed. */
+Delete from customer 
+where id =1;
+select * from customer;

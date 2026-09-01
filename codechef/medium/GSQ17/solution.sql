fc779b0825f5344efe6ee3ed1135138a17383fc4
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-01 21:21:18 +0530

    ✅ GSQ17 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-01

--- codechef/medium/GSQ17/solution.sql
+++ codechef/medium/GSQ17/solution.sql
@@ -1,1 +1,3 @@
-select * from Flights
+/* Write a query which does the following
+- Fetch the entry specifically from 2 columns - 'Passenger_name' and 'Gender' */
+select Passenger_name , Gender FROM Flights;

78a5e1993b46518947cafe2038c743504ffc3308
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-06 17:07:53 +0530

    ✅ LPDPR03 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/LPDPR03/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Creating a Series
+s = pd.Series([1, 3, 5, 7, 9])
+
+# Creating a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+
+print("Series:", s)
+print("\nDataFrame:", df)

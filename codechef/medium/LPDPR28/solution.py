de1f48360736b5d8835a02185af6869c4936d22a
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-09 20:38:13 +0530

    ✅ LPDPR28 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/LPDPR28/solution.py
+++ codechef/medium/LPDPR28/solution.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({'A': [1, 2, np.nan, 4],
-                   'B': [5, np.nan, np.nan, 8],
-                   'C': ['a', 'b', 'c', None]})
+df = pd.DataFrame({
+    'A': [1, 2, np.nan, 4, 5],
+    'B': [5, np.nan, np.nan, None, 10],
+    'C': ['a', 'b', 'c', None, 'e']
+})
                    
-# Update your code below this line
-print(df.isnull().sum()/len(df)*100)
+print(df)  
+
+# Drop rows where column A has missing values
+print(df.dropna(subset=['A']))
+
+# Drop rows with less than 2 non-null values
+print(df.dropna(thresh=2))
+
+# Drop rows where both Column A and Column B has missing values
+
+print(df.dropna(subset=['A','B'], how='all'))

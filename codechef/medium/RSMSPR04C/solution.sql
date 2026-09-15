a45342fb2063ac6567b8ecca42460e2b7f21f9ab
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-15 21:25:00 +0530

    ✅ RSMSPR04C | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-15

--- codechef/medium/RSMSPR04C/solution.sql
+++ codechef/medium/RSMSPR04C/solution.sql
@@ -1,3 +1,4 @@
-/* Update your query here*/update  products set stock_quantity=0
+/* Update your query here*/
+update  products set stock_quantity=0
 where stock_quantity =10;
 select * from products where stock_quantity=0;

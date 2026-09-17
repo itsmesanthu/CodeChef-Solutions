71530dc31a0528702a6984631178caa30ccfcdc7
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-17 22:48:27 +0530

    ✅ PYTH66 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/PYTH66/solution.py
+++ codechef/medium/PYTH66/solution.py
@@ -1,6 +1,4 @@
-b = int(input())
-r = int(input())
-if r>b:
-    print("Rob scored higher marks than Bob")
-elif r==b:
-    print("Bob & Rob both scored the same")
+a=int(input())
+b=int(input())
+if a>b:
+    print("Coding is Fun")

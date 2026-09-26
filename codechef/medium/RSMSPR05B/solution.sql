fa1aec22dba1cdea6373fd515e2160e2d6dd28ba
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-09-26 21:34:31 +0530

    ✅ RSMSPR05B | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-26

--- codechef/medium/RSMSPR05B/solution.sql
+++ codechef/medium/RSMSPR05B/solution.sql
@@ -2,6 +2,7 @@ BEGIN TRANSACTION;
 SAVEPOINT S1;
 --Savepoint created incase any changes to the the database beyond this point has to be undone in the future.
 
-/* Update your query below this line*/
-delete from products where stock_quantity=0;
-select product_id,name,stock_quantity from products;
+/* Update your query here*/
+delete from orders where
+order_date<'2024-01-20';
+select order_id, customer_id, order_date, total_amount  from orders;
